8feae1ca9c08aa856370a348bfa4dc378befcde9
Alfredo Padilla <doalfrepg@gmail.com>
2022-10-17 15:04:16 -0500

    actualizacion imp y cierre cto

--- GeneradorRutas/GeneradorRutas.py
+++ GeneradorRutas/GeneradorRutas.py
@@ -2,14 +2,21 @@ from openpyxl import load_workbook
 import pandas as pd
 
 archivoCargue = pd.read_excel('FEHCA_CIRCUITO_DPTO_ACCESO_IMP_CODIGO.xlsx')
-archivoCodigo = pd.read_excel('20220120_CODIGOS_CIRCUITO.xlsx')
+archivoCodigo = pd.read_excel('CODIGOS_CIRCUITO.xlsx')
 
 wb = load_workbook('FEHCA_CIRCUITO_DPTO_ACCESO_IMP_CODIGO.xlsx')
 datos = wb['DATOS_RUTA_TRABAJO']
 
+condicion_No= input("ACCESO_IMP - 0   CIERRE_CTO - 1  :")
+
+if condicion_No == 0:
+    condicion = "ACCESO_IMP"
+else:
+    condicion = " CIERRE_CTO"
+    
 seguir = "S"
 while (seguir == "S") or (seguir == "s") :
-    fecha = "20221004"
+    fecha = "20221012"
     circuito = "none"
     codigo = 0
     departamento = "none"
@@ -34,18 +41,17 @@ while (seguir == "S") or (seguir == "s") :
     print('MATRICULACION_' + str(codigo))
     print("MATRICULACIÓN")
     print(str(codigo))
-    print(str(xcircuito)+"_"+str(departamento)+"_ACCESO_IMP_R00"+str(NoRuta))
-    print(str(xcircuito)+"_"+str(departamento)+"_ACCESO_IMP_R00"+str(NoRuta))
+    print(str(xcircuito)+"_"+str(departamento)+"_"+condicion+"_R00"+str(NoRuta))
+    print(str(xcircuito)+"_"+str(departamento)+"_"+condicion+"_R00"+str(NoRuta))
     print(str(nombreKml))
     
     datos.cell(row=2,column=1).value = 'MATRICULACION_' + str(codigo) 
     datos.cell(row=2,column=2).value = "MATRICULACIÓN"
     datos.cell(row=2,column=3).value = str(codigo)
-    datos.cell(row=2,column=4).value = str(xcircuito)+"_"+str(departamento)+"_ACCESO_IMP_R00"+str(NoRuta)
-    datos.cell(row=2,column=5).value = str(xcircuito)+"_"+str(departamento)+"_ACCESO_IMP_R00"+str(NoRuta)
+    datos.cell(row=2,column=4).value = str(xcircuito)+"_"+str(departamento)+"_"+condicion+"_R00"+str(NoRuta)
+    datos.cell(row=2,column=5).value = str(xcircuito)+"_"+str(departamento)+"_"+condicion+"_R00"+str(NoRuta)
     datos.cell(row=2,column=6).value = str(nombreKml)
     
-    wb.save(fecha+'_'+xcircuito+'_'+departamento+'_ACCESO_IMP_'+str(codigo)+'.xlsx')
+    wb.save(fecha+'_'+xcircuito+'_'+departamento+'_'+condicion+'_'+str(codigo)+'.xlsx')
     
     seguir = input("crear otra ruta : ")
-    
